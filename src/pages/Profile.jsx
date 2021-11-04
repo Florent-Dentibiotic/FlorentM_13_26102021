12a85cafe4fbca3012d945b6d50f26806e15adb2
MacbookAir <fmdentiste@pm.me>
2021-11-04 11:28:21 +0100

    profile redirection if not login

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -4,12 +4,14 @@ import { selectUser, selectLogin } from '../Selectors/selector';
 import { editUserService } from '../services/EditUserService';
 import { userService } from '../services/UserService';
 import { setFirstName, setLastName } from '../features/userReducer';
+import { Redirect } from 'react-router-dom';
 
 export default function Profile() {
     // REDUX FUNCTIONS
     const store = useStore();
     const dispatch = useDispatch();
     const user = useSelector(selectUser);
+    const login = useSelector(selectLogin);
     const token = selectLogin(store.getState()).token;
 
     // REACT LOCAL FUNCTIONS
@@ -50,121 +52,143 @@ export default function Profile() {
 
     return (
         <>
-            {user.user_status === 'resolved' && (
+            {login.status === 'resolved' ? (
                 <>
-                    <main className="main bg-dark">
-                        <div className="header">
-                            {editProfile ? (
-                                <>
-                                    <h1>Welcome back</h1>
-                                    <form onSubmit={editNav}>
-                                        <div className="user-input">
-                                            <input
-                                                className="user-input-editor"
-                                                type="text"
-                                                id="username"
-                                                placeholder={user.firstName}
-                                                onChange={(e) =>
-                                                    setNewFirstName(
-                                                        e.target.value
-                                                    )
-                                                }
-                                            />
-                                            <input
-                                                className="user-input-editor"
-                                                type="text"
-                                                id="lastname"
-                                                placeholder={user.lastName}
-                                                onChange={(e) =>
-                                                    setNewLastName(
-                                                        e.target.value
-                                                    )
-                                                }
-                                            />
-                                        </div>
-                                        <div className="user-button">
+                    {user.user_status === 'resolved' && (
+                        <>
+                            <main className="main bg-dark">
+                                <div className="header">
+                                    {editProfile ? (
+                                        <>
+                                            <h1>Welcome back</h1>
+                                            <form onSubmit={editNav}>
+                                                <div className="user-input">
+                                                    <input
+                                                        className="user-input-editor"
+                                                        type="text"
+                                                        id="username"
+                                                        placeholder={
+                                                            user.firstName
+                                                        }
+                                                        onChange={(e) =>
+                                                            setNewFirstName(
+                                                                e.target.value
+                                                            )
+                                                        }
+                                                    />
+                                                    <input
+                                                        className="user-input-editor"
+                                                        type="text"
+                                                        id="lastname"
+                                                        placeholder={
+                                                            user.lastName
+                                                        }
+                                                        onChange={(e) =>
+                                                            setNewLastName(
+                                                                e.target.value
+                                                            )
+                                                        }
+                                                    />
+                                                </div>
+                                                <div className="user-button">
+                                                    <button
+                                                        className="user-edit-button"
+                                                        type="submit"
+                                                    >
+                                                        Save
+                                                    </button>
+                                                    <button
+                                                        className="user-edit-button"
+                                                        onClick={() =>
+                                                            setEditor(false)
+                                                        }
+                                                    >
+                                                        Cancel
+                                                    </button>
+                                                </div>
+                                            </form>
+                                        </>
+                                    ) : (
+                                        <>
+                                            <h1>
+                                                Welcome back
+                                                <br />
+                                                {user.firstName} {user.lastName}
+                                            </h1>
                                             <button
-                                                className="user-edit-button"
-                                                type="submit"
+                                                className="edit-button"
+                                                onClick={() => setEditor(true)}
                                             >
-                                                Save
+                                                Edit Name
                                             </button>
-                                            <button
-                                                className="user-edit-button"
-                                                onClick={() => setEditor(false)}
-                                            >
-                                                Cancel
-                                            </button>
-                                        </div>
-                                    </form>
-                                </>
-                            ) : (
-                                <>
-                                    <h1>
-                                        Welcome back
-                                        <br />
-                                        {user.firstName} {user.lastName}
-                                    </h1>
-                                    <button
-                                        className="edit-button"
-                                        onClick={() => setEditor(true)}
-                                    >
-                                        Edit Name
-                                    </button>
-                                </>
-                            )}
-                        </div>
-                        <h2 className="sr-only">Accounts</h2>
-                        <section className="account">
-                            <div className="account-content-wrapper">
-                                <h3 className="account-title">
-                                    Argent Bank Checking (x8349)
-                                </h3>
-                                <p className="account-amount">$2,082.79</p>
-                                <p className="account-amount-description">
-                                    Available Balance
-                                </p>
-                            </div>
-                            <div className="account-content-wrapper cta">
-                                <button className="transaction-button">
-                                    View transactions
-                                </button>
-                            </div>
-                        </section>
-                        <section className="account">
-                            <div className="account-content-wrapper">
-                                <h3 className="account-title">
-                                    Argent Bank Savings (x6712)
-                                </h3>
-                                <p className="account-amount">$10,928.42</p>
-                                <p className="account-amount-description">
-                                    Available Balance
-                                </p>
-                            </div>
-                            <div className="account-content-wrapper cta">
-                                <button className="transaction-button">
-                                    View transactions
-                                </button>
-                            </div>
-                        </section>
-                        <section className="account">
-                            <div className="account-content-wrapper">
-                                <h3 className="account-title">
-                                    Argent Bank Credit Card (x8349)
-                                </h3>
-                                <p className="account-amount">$184.30</p>
-                                <p className="account-amount-description">
-                                    Current Balance
-                                </p>
-                            </div>
-                            <div className="account-content-wrapper cta">
-                                <button className="transaction-button">
-                                    View transactions
-                                </button>
-                            </div>
-                        </section>
-                    </main>
+                                        </>
+                                    )}
+                                </div>
+                                <h2 className="sr-only">Accounts</h2>
+                                <section className="account">
+                                    <div className="account-content-wrapper">
+                                        <h3 className="account-title">
+                                            Argent Bank Checking (x8349)
+                                        </h3>
+                                        <p className="account-amount">
+                                            $2,082.79
+                                        </p>
+                                        <p className="account-amount-description">
+                                            Available Balance
+                                        </p>
+                                    </div>
+                                    <div className="account-content-wrapper cta">
+                                        <button className="transaction-button">
+                                            View transactions
+                                        </button>
+                                    </div>
+                                </section>
+                                <section className="account">
+                                    <div className="account-content-wrapper">
+                                        <h3 className="account-title">
+                                            Argent Bank Savings (x6712)
+                                        </h3>
+                                        <p className="account-amount">
+                                            $10,928.42
+                                        </p>
+                                        <p className="account-amount-description">
+                                            Available Balance
+                                        </p>
+                                    </div>
+                                    <div className="account-content-wrapper cta">
+                                        <button className="transaction-button">
+                                            View transactions
+                                        </button>
+                                    </div>
+                                </section>
+                                <section className="account">
+                                    <div className="account-content-wrapper">
+                                        <h3 className="account-title">
+                                            Argent Bank Credit Card (x8349)
+                                        </h3>
+                                        <p className="account-amount">
+                                            $184.30
+                                        </p>
+                                        <p className="account-amount-description">
+                                            Current Balance
+                                        </p>
+                                    </div>
+                                    <div className="account-content-wrapper cta">
+                                        <button className="transaction-button">
+                                            View transactions
+                                        </button>
+                                    </div>
+                                </section>
+                            </main>
+                        </>
+                    )}
                 </>
+            ) : (
+                <Redirect
+                    to={{
+                        pathname: `/`,
+                    }}
+                />
             )}
         </>
     );
