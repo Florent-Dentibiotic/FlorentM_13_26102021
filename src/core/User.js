b353bac1c43a4001a9ff780826e7dc4eef7f929d
MacbookAir <fmdentiste@pm.me>
2021-11-15 14:26:43 +0100

    Update User.js

--- src/core/User.js
+++ src/core/User.js
@@ -1,12 +1,12 @@
 export default class User {
-    /**
-     * Token informations
-     *
-     * @param { string } firstName
-     * @param { string } lastName
-     */
-    constructor(firstName, lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
+  /**
+   * User informations
+   *
+   * @param { string } firstName
+   * @param { string } lastName
+   */
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
 }
